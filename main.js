a1ccfee31ef1e8621be21a9363383db7d2c5e97a
Sergio Rojas <serg.rojas@tutamail.com>
2016-06-27 13:47:58 -0700

    Add a button to retrieve a new quote.

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
 var addScript = function(url) {
-  var script = document.createElement('script');
-  script.src = url;
+  var script  = document.createElement('script');
+  script.src  = url;
   document.body.appendChild(script);
+  document.body.removeChild(script);
 }
 var changeColor = function() {
-  var color  = Math.floor(Math.random()*16777215).toString(16);
-  console.log(color);
+  var color    = Math.floor(Math.random()*16777215).toString(16);
+  var quote    = document.getElementById('quote');
+  var author   = document.getElementById('author');
+  var newQuote = document.getElementById('new-quote');
+
+  document.body.style.backgroundColor = '#' + color;
+  quote.style.color                   = '#' + color;
+  author.style.color                  = '#' + color;
+  newQuote.style.backgroundColor      = '#' + color;
+
 }
 var getQuote = function() {
-  var url = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=parseQuote';
+  var url    = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=parseQuote';
   addScript(url);
 }
 var parseQuote = function(data) {
-  var quoteAuthor = data.quoteAuthor;
-  var quoteText   = data.quoteText;
-  console.log(quoteText);
-  console.log(quoteAuthor);
+  var quote    = document.getElementById('quote');
+  var author   = document.getElementById('author');
+  
+  var quoteAuthor  = data.quoteAuthor;
+  var quoteText    = data.quoteText;
 
+  quote.innerHTML  = quoteText;
+  author.innerHTML = quoteAuthor;  
+
+  changeColor();
 }
-changeColor();
-getQuote();
+
+function main() {
+  getQuote();
+}
